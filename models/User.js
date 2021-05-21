c97d457269ba770024ffdb35314faeab9caeb0c6
Charles <cchester1@angelo.edu>
2021-05-20 20:22:32 -0500

    worked on user-route.js

--- models/User.js
+++ models/User.js
@@ -34,7 +34,7 @@ User.init (
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-              len: [4]
+            len: [4]
             }
         }
     },
