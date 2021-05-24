341a52f5d1b0b878737ab62ef72deb2dcb019f01
KaleTEvans <kaletevans@gmail.com>
2021-05-24 17:55:30 -0500

    .

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,32 +3,36 @@ const { Team, TeamMember } = require('../models/')
 const sequelize = require('../config/connection');
 const { post } = require('./api');
 
-router.get('/', (req, res) => {
-  TeamMember.findAll({
-    attributes: [
-      'first_name',
-      'last_name',
-      'sports_team_name',
-      'position_played'
-    ],
-    include: [
-      {
-        models: Team,
-        attributes: [
-          'team_name',
-          'team_type'
-        ]
-      }
-    ]
-  })
-  .then(results => {
-    const teamMembers = results.map(teamMember => post.get({plain: true}) )
+// router.get('/', (req, res) => {
+//   TeamMember.findAll({
+//     attributes: [
+//       'first_name',
+//       'last_name',
+//       'sports_team_name',
+//       'position_played'
+//     ],
+//     include: [
+//       {
+//         models: Team,
+//         attributes: [
+//           'team_name',
+//           'team_type'
+//         ]
+//       }
+//     ]
+//   })
+//   .then(results => {
+//     const teamMembers = results.map(teamMember => post.get({plain: true}) )
 
-    res.render('homepage', {
-      teamMembers,
-      loggedIn: req.session.loggedIn
-    })
-  })
-});
+//     res.render('homepage', {
+//       teamMembers,
+//       loggedIn: req.session.loggedIn
+//     })
+//   })
+// });
+
+router.get('/', (req, res) => {
+  res.render('homepage');
+})
 
 module.exports = router;
