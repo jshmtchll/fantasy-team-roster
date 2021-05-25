05419daf5cff6cba1cfb9b6208bab95676c294cc
Charles <cchester1@angelo.edu>
2021-05-25 13:42:47 -0500

    blueprint for post.js

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -1,5 +1,12 @@
 async function sendPost(event) {
   event.preventDefault();
 
+  const // grab team-type
+  const //grab team-name
   
+  //going to send fetch request to team-routes
+  //method: post
+
 }
+
+//button addEventListener sendPost function
